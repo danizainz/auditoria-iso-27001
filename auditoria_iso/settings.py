c1adb482e601d3d4bf1798861cca0219f42108e3
danizainz <dani.carvalho2928@gmail.com>
2026-05-12 23:32:05 +0100

    mudei o email de suporte da plataforma

--- auditoria_iso/settings.py
+++ auditoria_iso/settings.py
@@ -131,13 +131,13 @@ SIMPLE_JWT = {
 
 
 
-# --- CONFIGURAÇÃO DE EMAIL (Já configurada para .env) ---
+# --- CONFIGURAÇÃO DE EMAIL ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT',587)
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_TIMEOUT = 10   # Se o Outlook não responder em 10 segundos, o Django desiste e não crasha o servidor
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_USER')
+EMAIL_TIMEOUT = 10 
